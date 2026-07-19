d5c695e289d04f5a81892686166421bed2b4da02
Junno Yuyn <yun00049@umn.edu>
2026-07-19 09:03:33 -0500

    Update project structure3

--- train_UDT_REPA.py
+++ train_UDT_REPA.py
@@ -16,21 +16,57 @@ from accelerate import Accelerator
 from accelerate.logging import get_logger
 from accelerate.utils import ProjectConfiguration, set_seed
 
-from loss.loss_REPA import SILoss                                     # Loss
+from loss.loss_REPA import SILoss                           # Loss
 from dataset import CustomDataset_REPA  as CustomDataset    # Dataset
 from diffusers.models import AutoencoderKL                  # Model(VAE)
-from models.UDT_REPA import UDT_models                           # Model(UDT)
+from models.UDT_REPA import UDT_models                      # Model(UDT)
 
 import wandb
 import math
 from torchvision.utils import make_grid
 import time
 import datetime
-from utils_REPA import load_encoders
 from timm.data import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
 from torchvision.transforms import Normalize
 
 logger = get_logger(__name__)
+
+
+@torch.no_grad()
+def load_encoders(enc_type, device, resolution=256):
+    assert (resolution == 256) or (resolution == 512)
+    
+    enc_names = enc_type.split(',')
+    encoders, architectures, encoder_types = [], [], []
+    for enc_name in enc_names:
+        encoder_type, architecture, model_config = enc_name.split('-')
+        if resolution == 512:
+            if encoder_type != 'dinov2':
+                raise NotImplementedError(
+                    "Currently, we only support 512x512 experiments with DINOv2 encoders."
+                    )
+
+        architectures.append(architecture)
+        encoder_types.append(encoder_type)
+        if 'dinov2' in encoder_type:
+            import timm
+            if 'reg' in encoder_type:
+                encoder = torch.hub.load('facebookresearch/dinov2', f'dinov2_vit{model_config}14_reg')
+            else:
+                encoder = torch.hub.load('facebookresearch/dinov2', f'dinov2_vit{model_config}14')
+            del encoder.head
+            patch_resolution = 16 * (resolution // 256)
+            encoder.pos_embed.data = timm.layers.pos_embed.resample_abs_pos_embed(
+                encoder.pos_embed.data, [patch_resolution, patch_resolution],
+            )
+            encoder.head = torch.nn.Identity()
+            encoder = encoder.to(device)
+            encoder.eval()
+        
+
+        encoders.append(encoder)
+    
+    return encoders, encoder_types, architectures
 
 
 def preprocess_raw_image(x, enc_type):
